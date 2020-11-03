f337b4c5de0d55c602e5dd4a191f092f6dbfbd2e
Charles Mogayzel <mogayzelc@gmail.com>
2020-11-03 13:55:46 -0800

    Added server listener

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const bodyParser = require('body-parser');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +24,6 @@ const server = app.listen(port, listening);
 
 function listening() {
     console.log("server running");
-    console.log(`running on localhost: ${$port}`);
-    
+    console.log(`running on localhost: ${port}`);
+
 }
