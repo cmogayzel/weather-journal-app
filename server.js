92f7e289769863eb30da155e93bc5aed99cc43c5
Charles Mogayzel <mogayzelc@gmail.com>
2020-11-05 14:54:24 -0800

    modified server js code

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const { response } = require("express");
+const { request } = require("http");
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
+const app = express();
+
+// Dependencies
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -19,9 +26,29 @@ app.use(express.static('website'));
 // Setup Server
 const port = 8080;
 
+//start server
+const server = app.listen(port,listening)
+
+function listening() {
+    console.log('server is running');
+    console.log(`running on localhost: ${port}`);
+}
+
+
 //get function 
-app.get('/allData', sendData)
+app.get('/all', sendData)
 
 function sendData(req, res) {
     res.send(projectData);
+}
+
+//Route POST
+app.post('/weatherData',postData);
+
+function postData(req,res) {
+    projectData.temperature = req.body.temperature;
+    projectData.date = req.bod.date;
+    projectData.user_response = req.body.user_response;
+    res.end();
+    console.log(projectData);
 }
